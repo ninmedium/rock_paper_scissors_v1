806325bfd382add9b5b23a62b7dcde6426264cd2
Nigel <ninmedium@gmail.com>
2024-12-08 10:51:57 +0800

    Create primary random RPS function version 1
    - returns the string "rock", "paper" or "scissors"

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let randNum = 3 * Math.random();
+
+    if (randNum <= 1) {
+        return "rock";
+    } else if (randNum <= 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
